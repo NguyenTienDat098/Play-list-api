ff2404702bae7ed9f2c04738026c50c9db12db6a
NguyenTienDat098 <dat61222@gmail.com>
2022-11-26 11:11:21 +0700

    test deploy vercel

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const product = require("./api/product");
 const app = express();
 const db = require("./configs/dbConfig");
 const path = require("path");
+//db.connect();
 app.use(express.json({ extended: false }));
 app.use("/build", express.static(path.join(__dirname + "/build/")));
 app.use("/", product);
-db.connect();
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
