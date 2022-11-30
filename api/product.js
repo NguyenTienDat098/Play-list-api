89a4d11ac560bf984a883f5d9022fa7dc25f4859
NguyenTienDat098 <dat61222@gmail.com>
2022-11-30 18:36:16 +0700

    my playlist -server config deploy vercel server test 30

--- api/product.js
+++ api/product.js
@@ -13,6 +13,8 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 router.post("/api/uploads", upload.single("myFile"), (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   UploadController.createPlaylist(req, res, next);
   console.log(req.file.originalname + " file successfully uploaded !!");
 });
